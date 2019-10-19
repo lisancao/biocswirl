4c4569794cb60eb22796f95858f1f9102fa6cce2
Kate <kate.tyshchenko@yandex.ru>
2019-10-19 11:09:51 -0700

    Add course list function documentation

--- R/list_courses.R
+++ R/list_courses.R
@@ -1,3 +1,11 @@
+#' @title Course list function
+#'
+#' @description This function lists all the course titles available.
+#' @keywords course_list
+#' @export
+#' @examples
+#' list_courses()
+
 list_courses <- function(){
 	url <- "https://api.github.com/repos/lisancao/biocswirl/git/trees/f401ca47303e16d67ff8c24f035c9e2b4e0a1598"
 	get_url <- httr::GET(url)
